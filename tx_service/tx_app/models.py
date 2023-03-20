af2069e63301032fe163748740a2e48999d79cb6
Elliot Harding <elliot@hardings.org.uk>
2023-03-20 20:05:52 +0000

    Add Initial Models

--- tx_service/tx_app/models.py
+++ tx_service/tx_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Account(models.Model):
+    resource_id = models.CharField(max_length=200)
+    owner_name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Transaction(models.Model):
+    transaction_id = models.CharField(max_length=200)
+    internal_transaction_id = models.CharField(max_length=200)
+
+    booking_date = models.DateField()
+    value_date = models.DateField()
+    booking_date_time = models.DateTimeField()
+    value_date_time = models.DateTimeField()
+
+    amount = models.FloatField()
+    currency = models.CharField(max_length=10)
+
+    creditorName = models.CharField(max_length=200)
+    transactions_code = models.CharField(max_length=10)
+    
+    account = models.ForeignKey(Account, models.CASCADE)
+
+
